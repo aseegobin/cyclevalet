8e70cc2ef71c395fcc5c584793a90bc43ba577f2
Avie Seegobin <avieseegobin@gmail.com>
2017-03-14 18:22:35 -0400

    prox. sensor in locking code

--- lockingmech/lockingmech.ino
+++ lockingmech/lockingmech.ino
@@ -14,23 +14,24 @@ int last_deg = 0;
 bool lock_down = false;
 
 // Input pin declaration
-int inPin = 3;
-int inPin_2 = 2;
+int pirPin = 3;
+int intSwitchPin = 2;
 
 // Variable storage for input value
-int val = 0;
-int val_2 = 0;
+int pirReadVal = 0;
+int intSwitchReadVal = 0;
+
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;
 
 void setup(void)
 {
   Serial.begin(115200);
 
-
+  pinMode(pirPin, INPUT);    // declare pirPin as input
+  pinMode(intSwitchPin, INPUT);    // declare pushbutton as input
   nfc.begin();
   Serial.println("MF1S50 Reader Demo From Elechouse!");
-
-  // attaches the servo on pin 9 to the servo object
-  myservo_9.attach(9);
 
   uint32_t versiondata = nfc.get_version();
   while (!versiondata) {
@@ -45,11 +46,20 @@ void setup(void)
   Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
 
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+    }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+
   Serial.println("Starting locking program");
 
-  pinMode(inPin, INPUT);    // declare pushbutton as input
-  pinMode(inPin_2, INPUT);    // declare pushbutton as input
   pinMode(13, OUTPUT);
+  // attaches the servo on pin 9 to the servo object
+  myservo_9.attach(9);
   myservo_9.write(start_deg);
 
   /** Set normal mode, and disable SAM */
@@ -61,11 +71,11 @@ void loop(void)
 
   u8 buf[32], sta;
 
-  val = digitalRead(inPin);
-  val_2 = digitalRead(inPin_2);
+  pirReadVal = digitalRead(pirPin);
+  intSwitchReadVal = digitalRead(intSwitchPin);
 
-  if (val == HIGH) {
-    Serial.println("read push button, going to read nfc...");
+  if (pirReadVal == HIGH) {
+    Serial.println("read prox sensor, going to read nfc...");
     digitalWrite(13, HIGH);
 
     /** Polling the mifar card, buf[0] is the length of the UID */
@@ -99,8 +109,8 @@ void loop(void)
             myservo_9.write(i);
             delay(50);
 
-            val_2 = digitalRead(inPin_2);
-            if (val_2 == HIGH) {
+            intSwitchReadVal = digitalRead(intSwitchPin);
+            if (intSwitchReadVal == HIGH) {
 
               Serial.println("----INTERFERENCE DETECTED-----");
               delay(100);
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,20 +1,9 @@
 //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 30;        
-
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;         
-
-//the amount of milliseconds the sensor has to be low 
-//before we assume all motion has stopped
-long unsigned int pause = 5000;  
-
-boolean lockLow = true;
-boolean takeLowTime;  
+int calibrationTime = 30;
 
 int pirPin = 2;    //the digital pin connected to the PIR sensor's output
 int ledPin = 13;
 int readVal = 0;
-int oldReadVal = 0;
 
 
 /////////////////////////////
@@ -30,23 +19,21 @@ void setup(){
     for(int i = 0; i < calibrationTime; i++){
       Serial.print(".");
       delay(1000);
-      }
-    Serial.println(" done");
-    Serial.println("SENSOR ACTIVE");
-    delay(50);
-  }
+    }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+  delay(50);
+}
 
-////////////////////////////
-//LOOP
 void loop(){
-     digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
-     readVal = digitalRead(pirPin);
-     
-      while(readVal == HIGH){
-        digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
-        Serial.println("DETECT");
-        readVal = digitalRead(pirPin);
-     }
-     delay(50);
-     Serial.println("NONE");
+  digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
+  readVal = digitalRead(pirPin);
+
+  while(readVal == HIGH){
+    digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+    Serial.println("DETECT");
+    readVal = digitalRead(pirPin);
   }
+  delay(50);
+  Serial.println("NONE");
+}
